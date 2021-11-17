2c8ab80b952eb8dd925a513c71afe0ce4fb458e8
Damian.Ujma@hyland.com <Damian.Ujma@hyland.com>
2021-11-17 13:19:37 +0100

    ACS-2265 Fix removed job executor in ActivitiEngineInitializer

--- repository/src/main/java/org/alfresco/repo/workflow/activiti/ActivitiEngineInitializer.java
+++ repository/src/main/java/org/alfresco/repo/workflow/activiti/ActivitiEngineInitializer.java
@@ -55,16 +55,16 @@ public class ActivitiEngineInitializer extends AbstractLifecycleBean
     	
     	if (workflowAdminService.isEngineEnabled(ActivitiConstants.ENGINE_ID)) 
     	{
-    		((ProcessEngineImpl)processEngine).getProcessEngineConfiguration().init();
+    		((ProcessEngineImpl)processEngine).getProcessEngineConfiguration().getAsyncExecutor().start();
     	}
     }
 
 	@Override
 	protected void onShutdown(ApplicationEvent event) {
 		if(workflowAdminService.isEngineEnabled(ActivitiConstants.ENGINE_ID) && 
-				((ProcessEngineImpl)processEngine).getProcessEngineConfiguration().getJobExecutor().isActive())
+				((ProcessEngineImpl)processEngine).getProcessEngineConfiguration().getAsyncExecutor().isActive())
 		{
-			((ProcessEngineImpl)processEngine).getProcessEngineConfiguration().getJobExecutor().shutdown();
+			((ProcessEngineImpl)processEngine).getProcessEngineConfiguration().getAsyncExecutor().shutdown();
 		}
 	}
 }
